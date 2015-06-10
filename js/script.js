c7ca84d7c641514d17906b3849ada52ffc5a1edd
Nazarett <gnazarett@gmail.com>
2015-06-10 00:57:02 -0400

    Practice

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Dog years exercise.
+//Calculate Sparky's age in dog years.
+
+var sparkysHumanYears = 21; //Sparky's years in human years.
+
+// 1 human year = 7 dog years.
+//multiplying human years * 7 ( dog years for every human years) should give us Sparky's age.
+
+console.log(sparkysHumanYears * 7 + " Dog years" ); // This is the math operation to know Sparky years in dog years.
+var dogYears = 147;
+
+
+
+//Slice of pie part 1
+
+var numOfPizzas = 15; //Number of pizzas ordered.
+var numOfSlices = 8; //Number of slices per pizza.
+var people = 19; //Number of people at the party.
+var totalSLices = numOfSlices * numOfPizzas; //This will calculate the total slices of all pizzas.
+var slicesPerPerson = totalSLices / people; //This will calculate the slices per person.
+//Calculate the total of slices by multiplying the amount of slices per pizza * the total amount of pizza.
+
+console.log(totalSLices + " slices of pizza"); //This operation will give us the total amount of slices.
+var totalSlices = 120;//Total slices of 8 pizzas
+
+//Calculate how many slices per person by dividing the total slices / the total amount of people at the party.
+
+console.log(slicesPerPerson + " slices of pizza per person");// Total of slices of pizza per person at the party.
+var pizzasPerPerson = 6.3 //Slice of pizza corresponding to each person in the party.
+
+
+//Slice of pie 2
+// Calculate how many slices will sparky get after pizza is evenly divided among the guest.
